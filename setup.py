ee844aadedaf4075496776d27eb695b81cf4def3
Sebastian Paripsa <sparipsa@digon.io>
2021-10-18 20:11:55 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,49 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-VERSION = "0.0.1"
+CLASSIFIERS = """\
+LICENSE :: OSI Approved
+"Programming Language :: Python :: 3"
+"Intended Audience :: Science/Research"
+"Operating System :: OS Independent"
+"Development Status :: 1 - Development"
+"""
+
+DISTNAME = "ginRex"
+VERSION = "0.1.0"
+ISRELEASED = False
 DESCRIPTION = "Python package ginRex"
 LONG_DESCRIPTION = "Python package for grazing incidene time-resolved Exafs"
+LICENSE = "MIT"
+AUTHOR = "Sebastian Paripsa"
+AUTHOR_EMAIL = "sebastian.paripsa@uni-wuppertal.de"
 
+# PYTHON_REQUIRES = "3"
 
-setup(
-    name="ginrex",
+# INSTALL_REQUIRES = [
+# "numpy",  # "xraylarch", # add any additional packages that
+# needs to be installed along with your package.
+# ]
+
+PACKAGES = find_packages
+
+metadata = dict(
+    name=DISTNAME,
     version=VERSION,
-    author="Sebastian Paripsa",
-    author_email="sebastian.paripsa@uni-wuppertal.de",
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    packages=find_packages(),
+    # python_requires=PYTHON_REQUIRES,
+    # install_requires=INSTALL_REQUIRES,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
-    packages=find_packages(),
-    install_requires=["xraylarch",],  # add any additional packages that
-    # needs to be installed along with your package.
-    classifiers=[
-        "Development Status :: 1 - Planning",
-        "Intended Audience :: Science/Research",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
+    classifiers=[CLASSIFIERS],
+    license=LICENSE,
 )
+
+
+def setup_package() -> None:
+    setup(**metadata)
+
+
+if __name__ == "__main__":
+    setup_package()
