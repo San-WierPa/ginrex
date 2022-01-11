c2f8819b771de786b80e9ce25abbe9369c2af1a0
Sebastian Paripsa <sparipsa@digon.io>
2022-01-11 15:16:45 +0100

    Add class

--- ginrex/splitter.py
+++ ginrex/splitter.py
@@ -1,19 +1,35 @@
+"""
+This module provides a class of various methods of the c# Splitter-file
+from the TREX.dll.
+"""
+
 import clr
 
 clr.AddReference(
     "C:\\Users\\yuli.pari\\Desktop\\gitUni\\ginRex\\ginrex\\dlls\\TREX.dll"
 )
-
 from TREX import Splitter
 
 
-def counting_rows(sourcefile) -> int:
+class PySplitter:
     """
-    Delivers the number of rows of a given file.
+    Delegates all method calls to "Splitter"-file from the TREX.dll
+    """
 
-    Parameters
-    ----------
-    sourcefile: String
-        Datafile under investigation.
-    """
-    return Splitter.CountingRows(sourcefile)
+    def __init__(self) -> None:
+        self.splitter = Splitter()
+
+    # def __getattribute__(self, __name: str) -> Any:
+    #    pass
+
+    def counting_rows(filename) -> int:
+        """
+        Delivers the number of rows of a given file.
+
+        Parameters
+        ----------
+        filename: String
+            Datafile under investigation, which is automatically copied from the
+            initial loading of a datafile.
+        """
+        return Splitter.CountingRows(filename)
